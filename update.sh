b4158ad3a58a5729a9eea1129afcfe2fe0179303
Marc Nawman <mnawman@proton.me>
2026-08-26 01:10:01 +0200

    Zsh config file

--- update.sh
+++ update.sh
@@ -2,10 +2,13 @@
 # Vim config
 cp ~/.vimrc .
 cp ~/.plugins.vimrc .
+# Ohmyzsh
+cp ~/.zshrc .
 # YCM
 cp ~/.vim/.ycm_extra_conf.py .vim/
 
 # i3 config
 cp ~/.config/i3/config .config/i3/
 # Polybar config
-cp ~/.config/polybar/config.ini .config/polybar/
+# cp ~/.config/polybar/config.ini .config/polybar/
+
